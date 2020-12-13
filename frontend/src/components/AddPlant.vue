a3166e2d82f80908a77a7d3c40b48312f36cb326
CodingIsLove <christopher.germann.eth@gmail.com>
2020-12-13 23:16:10 +0100

    Basis configuration to get started with Frontend

---
+++ frontend/src/components/AddPlant.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="addPlant">
+    <h1>This here is the div for the add Plant Element</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "addPlant",
+}
+</script>
+
+<style scoped>
+
+</style>

4fe38d0d9ed59a5fcd7663f469260e419bbba4fd
CodingIsLove <christopher.germann.eth@gmail.com>
2021-06-06 17:39:26 +0200

    created blueprint for the cosmosdb rest api

--- backend/src/router/cosmosDbRouter.ts
+++ backend/src/router/cosmosDbRouter.ts
@@ -2,8 +2,38 @@ import {Request, Response} from "express";
 import express from 'express';
 const cosmosDbRouter = express.Router()
 
-cosmosDbRouter.get('/',function(req:Request,res:Response){
-    res.send("Fuck yes it was working as expected")
+
+/**
+ * Get Requests
+ */
+cosmosDbRouter.get('/envSamples',function(req:Request,res:Response){
+    res.status(501).send("Function is not implemented yet")
+})
+
+cosmosDbRouter.get('/db', function (req:Request, res:Response){
+    res.status(501).send("Function is not implemented yet")
+})
+
+/**
+ * Post Requests
+ */
+cosmosDbRouter.post('/createDb',function(req:Request,res:Response){
+    res.status(501).send("Function is not implemented yet")
+})
+
+cosmosDbRouter.post('/envSample',function(req:Request,res:Response){
+    res.status(501).send("Function is not implemented yet")
+})
+
+/**
+ *  Delete Requests
+ */
+cosmosDbRouter.delete('/envSample',function(req:Request,res:Response){
+    res.status(501).send("Function is not implemented yet")
+})
+
+cosmosDbRouter.delete('/deleteDb',function(req:Request,res:Response){
+    res.status(501).send("Function is not implemented yet")
 })
 
 export default cosmosDbRouter

f8b0e96f968bc50c159d725290c9a42a603836a1
CodingIsLove <christopher.germann.eth@gmail.com>
2021-06-12 22:08:25 +0200

    First try out with http module, instead of the deprecated restler package

--- backend/src/tests/cosmosDbTest.ts
+++ backend/src/tests/cosmosDbTest.ts
@@ -1,41 +1,58 @@
 import chai from 'chai'
 import {describe, it, before, after} from 'mocha'
+import http from 'http'
 import rest from 'restler'
 
 const should = chai.should()
 const baseUrl = 'http://localhost:8081'
 
-
-
 describe('Basic CosmosDB Transactions', function () {
     it('should create a test database', function (done) {
-        rest.post(`${baseUrl}/cosmosDb/createDb`,
-            {data: {
-                dbName: 'TestDataBase'
-                }})
-            .on('fail', (data, response)=>{
-               throw new Error('Successfull Request, but failed to create db')
-            })
-            .on('success', (data, response)=>{
-                done()
-            })
-            .on('error', (err)=>{
-               throw new Error('Error occured: When creating db')
-            })
+        const postData = JSON.stringify({
+            dbName: 'TestDatabase'
+        })
+        const req = http.request({
+                port: 8081,
+                hostname: 'http://localhost',
+                path: '/cosmosDb/createDb',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': Buffer.byteLength(postData)
+                }
+            }, (res) => {
+                res.setEncoding('utf-8')
+                res.on('data', (chunk) => {
+                    console.log(`BODY: ${chunk}`);
+                    done()
+                });
+                res.on('end', () => {
+                    console.log('No more data in response.');
+                    done()
+                });
+            }
+        )
+
+        req.on('error', (e)=>{
+            console.error('Fuck something went not as expected')
+        })
+
     })
     it('should create a new Seed Container', function (done) {
         rest.post(`${baseUrl}/cosmosDb/createContainer`,
-            {data: {
+            {
+                data: {
                     dbName: 'TestDataBase',
-                    containerName:'Seed'
-                }})
-            .on('fail', (data, response)=>{
+                    containerName: 'Seed'
+                }
+            })
+            .on('fail', (data, response) => {
                 throw new Error('Successfull Request, but failed to create container')
             })
-            .on('success', (data, response)=>{
+            .on('success', (data, response) => {
                 done()
             })
-            .on('error', (err)=>{
+            .on('error', (err) => {
                 throw new Error('Error occured: When creating container')
             })
     })
